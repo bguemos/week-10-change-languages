550978635799225c598aa47ba5afd8f55280a5a7
bguemos <bri-lynn@hotmail.com>
2023-03-15 20:54:53 -0700

    updating language

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "080d5353d52b3510"; }
+/******/ 		__webpack_require__.h = function() { return "c599d0481bc54442"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
